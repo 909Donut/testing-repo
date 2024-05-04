e269ae621580050c91afee4fd0410213d0d05d19
909Donut <909donut@gmail.com>
2024-05-04 08:32:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send("<h1>This is an express project</h1>")
+})
+
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+})
